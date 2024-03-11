14e810e6c55fdd9c08e4de6ad549ccb3bb8b02d2
baksihwan <baksh999@naver.com>
2024-03-11 21:35:15 +0900

    mbti,switch 표현식, break생략

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -1,12 +1,24 @@
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-  @GetMapping("/main/1")
+
+  @GetMapping("/mbti/{name}")
   @ResponseBody
-  public String main(){
-    return "스프링부트 첫 수업";
+  public String showMbti(@PathVariable String name) {
+    System.out.println(name);
+
+    String rs = switch (name) {
+      case "홍길동" -> "INFP";
+      case "홍길순" -> "ENFP";
+      case "임꺽정" -> "ESFJ";
+      case "신짱구" -> "ENTP";
+      default -> "모름";
+    };
+
+    return rs;
   }
 }
--- src/main/java/com/sbs/exam/sbb/SbbApplication.java
+++ src/main/java/com/sbs/exam/sbb/SbbApplication.java
@@ -2,8 +2,9 @@ package com.sbs.exam.sbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude={DataSourceAutoConfiguration.class})
 public class SbbApplication {
 
 	public static void main(String[] args) {
